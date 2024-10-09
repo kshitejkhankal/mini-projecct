ee7e3ed1ebe0ec09df953c8a622dc720fb2fe51b
kshitejkhankal <kshitejkhankal@gmsil.com>
2024-10-09 15:59:44 +0530

    last update role

--- iam-role.tf
+++ iam-role.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
+  name = "ecsservicerole"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
