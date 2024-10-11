49aa6df1ebfd427a8ec8961deb6bdfe438d42f47
kshitejkhankal <kshitejkhankal@gmsil.com>
2024-10-11 09:45:58 +0530

    update task

--- task-diff.tf
+++ task-diff.tf
@@ -11,7 +11,7 @@ resource "aws_ecs_task_definition" "python-project" {
 [
   {
     "name": "python-project-container",
-    "image": "${aws_ecr_repository.python-project.repository_url}:17",
+    "image": "${aws_ecr_repository.python-project.repository_url}:18",
     "cpu": 1024,
     "memory": 2048,
     "essential": true,
